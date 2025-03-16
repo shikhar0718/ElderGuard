6c8360466262c4af796d32d0440dfe676a41529b
shikhar0718 <152046670+shikhar0718@users.noreply.github.com>
2025-03-17 00:14:02 +0530

    Create registration.js

---
+++ registration.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC_KYub-19ri6TJi3jK8abnTn0Qkc4GvTc",
+    authDomain: "elderguard-21369.firebaseapp.com",
+    databaseURL: "https://elderguard-21369-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "elderguard-21369",
+    storageBucket: "elderguard-21369.firebasestorage.app",
+    messagingSenderId: "201757709021",
+    appId: "1:201757709021:web:576a8f9aead45cb16c4134",
+    measurementId: "G-Z4E77KYKTL"
+  };
